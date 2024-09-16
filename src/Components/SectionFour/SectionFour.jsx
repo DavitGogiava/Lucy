356e513f16f541b2d6f8f5365f14c8263fa9d7d3
DavitGogiava <ursamajor027@gmail.com>
2024-09-16 04:05:35 -0700

    new

--- src/Components/SectionFour/SectionFour.jsx
+++ src/Components/SectionFour/SectionFour.jsx
@@ -7,7 +7,7 @@ const SectionFour = () => {
                 Contact Me.
             </div>
             <div className="other-info">
-                <h3>TestDesignDG@gmail.com</h3>
+                <h3>Website made by Davit Gogiava</h3>
                 <h3>Tbilisi, Georgia</h3>
             </div>
             <div className="contact-list">
